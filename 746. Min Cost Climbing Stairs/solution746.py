2dc4e4ae810274c01520f8ff02bf7589c587cadc
ajeandouble <jean.adrien.mtpl@gmail.com>
2025-04-15 21:56:51 +0200

    + 2dp

--- 746. Min Cost Climbing Stairs/solution746.py
+++ 746. Min Cost Climbing Stairs/solution746.py
@@ -1,28 +1,25 @@
 from typing import List
 
+
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        dp = [0 for i in range(0, n+2)]
-        dp[n] = 0
-        if len(cost) == 2:
-            return cost[0] if cost[0] < cost[1] else cost[1]
-        for i in range(n-1, -1, -1):
-            dp[i] = cost[i] + min(dp[i+2], dp[i+1])
-            print(dp, f'\t\tcost[{i}]={cost[i]}')
+        ans = 0
+        N = len(cost)
+        for i in range(2, N):
+            min_prev_step = min(cost[i - 2], cost[i - 1])
+            ans += min_prev_step
+            cost[i] += min_prev_step
+        return min(cost[-2], cost[-1])
 
-        return min(dp[0], dp[1])
 
-s = Solution()
+sol = Solution()
 
-# lst = [10, 15, 20]
-# ans = s.minCostClimbingStairs(lst)
-# print(f'{lst} -> {ans   }')
+cost = [10, 15, 20]
+ans = sol.minCostClimbingStairs(cost)
+print(f"{cost} => {ans}")
+assert ans == 15
 
-# lst = [1,100,1,1,1,100,1,1,100,1]
-# ans = s.minCostClimbingStairs(lst)
-# print(f'{lst} -> {ans   }')
-
-lst = [1,100,1,1,1,100,1]
-ans = s.minCostClimbingStairs(lst)
-print(f'{lst} -> {ans   }')
+cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+ans = sol.minCostClimbingStairs(cost)
+print(f"{cost} => {ans}")
+assert ans == 6
