c4419bc02d7dc677afe5abd103fc58e4bfe55380
ajeandouble <34901348+ajeandouble@users.noreply.github.com>
2025-04-23 14:06:37 +0000

    Perfect square!

--- 279. Perfect Squares/solution279.py
+++ 279. Perfect Squares/solution279.py
@@ -1,125 +1,27 @@
+from math import sqrt
+from math import inf
+
+
 class Solution:
     def numSquares(self, n: int) -> int:
-        squares = [
-            1,
-            4,
-            9,
-            16,
-            25,
-            36,
-            49,
-            64,
-            81,
-            100,
-            121,
-            144,
-            169,
-            196,
-            225,
-            256,
-            289,
-            324,
-            361,
-            400,
-            441,
-            484,
-            529,
-            576,
-            625,
-            676,
-            729,
-            784,
-            841,
-            900,
-            961,
-            1024,
-            1089,
-            1156,
-            1225,
-            1296,
-            1369,
-            1444,
-            1521,
-            1600,
-            1681,
-            1764,
-            1849,
-            1936,
-            2025,
-            2116,
-            2209,
-            2304,
-            2401,
-            2500,
-            2601,
-            2704,
-            2809,
-            2916,
-            3025,
-            3136,
-            3249,
-            3364,
-            3481,
-            3600,
-            3721,
-            3844,
-            3969,
-            4096,
-            4225,
-            4356,
-            4489,
-            4624,
-            4761,
-            4900,
-            5041,
-            5184,
-            5329,
-            5476,
-            5625,
-            5776,
-            5929,
-            6084,
-            6241,
-            6400,
-            6561,
-            6724,
-            6889,
-            7056,
-            7225,
-            7396,
-            7569,
-            7744,
-            7921,
-            8100,
-            8281,
-            8464,
-            8649,
-            8836,
-            9025,
-            9216,
-            9409,
-            9604,
-            9801,
-            10000,
-        ]
-        squares = list(filter(lambda x: x <= n, squares))[::-1]
-        N = len(squares)
-        
-        ans = n
-        def bfs(remaining, l, i):
-            # print(remaining, l, i)
-            while i < N and remaining - squares[i] >= 0:
-                if remaining % squares[i] == 0:
-                    print('yo')
-                    nonlocal ans
-                    if l + (remaining // squares[i]) >= ans: return None 
-                    ans = min(ans, l + (remaining // squares[i]))
-                bfs(remaining - squares[i], l + 1, i + 1)
-                i += 1
-            return None
-        bfs(n, 0, 0)
-        return ans
+        dp = [i for i in range(n + 1)]
+        max_sqrt = int(sqrt(n))
+        for i in range(2, max_sqrt + 1):
+            square = i * i
+            for j in range(square, n + 1):
+                dp[j] = min(dp[j], dp[j - square] + 1)
+        return dp[n]
 
 
-ans = Solution().numSquares(120)
-print(f"ans -> {ans}")
+ans = Solution().numSquares(1)
+assert ans == 1  # 1
+ans = Solution().numSquares(2)
+assert ans == 2  # 1 + 1
+ans = Solution().numSquares(3)
+assert ans == 3  # 1 + 1 + 1
+ans = Solution().numSquares(4)
+assert ans == 1  # 4
+ans = Solution().numSquares(12)
+assert ans == 3  # 4 + 4 + 4
+ans = Solution().numSquares(14)
+assert ans == 3  # 1 + 1 + 4 + 4 + 4
