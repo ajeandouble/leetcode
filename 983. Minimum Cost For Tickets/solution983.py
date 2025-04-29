79d451b652e901b1c0f1543cf89fbb6d88269177
ajeandouble <34901348+ajeandouble@users.noreply.github.com>
2025-04-29 05:36:54 +0000

    Minimum Cost For Tickets

---
+++ 983. Minimum Cost For Tickets/solution983.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        N, ans = len(days), 0
+        if N == 1:
+            return min(costs)
+        dp = [0] * 366
+        i = 0
+        while i < N:
+            d = days[i]
+            step_back_1 = (dp[d - 1] if d - 1 > 0 else 0) + costs[0]
+            step_back_7 = (dp[d - 7] if d - 7 > 0 else 0) + costs[1]
+            step_back_30 = (dp[d- 30] if d - 30 > 0 else 0) + costs[2]
+            dp[d] += min(step_back_1, step_back_7, step_back_30)
+            j = d + 1
+            while i + 1 < N and j < days[i + 1]:
+                dp[j] = dp[d]
+                j += 1
+            i += 1
+        return dp[d]
