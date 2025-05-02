77d8023b28e7fe0556145de0c22acc19d06f5008
ajeandouble <34901348+ajeandouble@users.noreply.github.com>
2025-05-02 17:57:31 +0000

    Longest common subsequence

--- 174. Dungeon Game/solution174.py
+++ 174. Dungeon Game/solution174.py
@@ -4,27 +4,31 @@ from typing import List
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         ROWS, COLS = len(dungeon), len(dungeon[0])
-        dp = [[0 for _ in range(COLS)] for _ in range(ROWS)]
-        dp[-1][-1] = 1 if dungeon[-1][-1] >= 0 else abs(dungeon[-1][-1]) + 1
+        dungeon[-1][-1] = 1 if dungeon[-1][-1] >= 0 else abs(dungeon[-1][-1]) + 1
         for col in range(COLS - 2, -1, -1):
-            if dungeon[-1][col] - dp[-1][col + 1] >= 0:
-                dp[-1][col] = 1
+            if dungeon[-1][col] - dungeon[-1][col + 1] >= 0:
+                dungeon[-1][col] = 1
             else:
-                dp[-1][col] = abs(dungeon[-1][col] - dp[-1][col + 1])
+                dungeon[-1][col] = abs(dungeon[-1][col] - dungeon[-1][col + 1])
 
         for row in range(ROWS - 2, -1, -1):
-            if dungeon[row][-1] - dp[row + 1][-1] >= 0:
-                dp[row][-1] = 1
+            if dungeon[row][-1] - dungeon[row + 1][-1] >= 0:
+                dungeon[row][-1] = 1
             else:
-                dp[row][-1] = abs(dungeon[row][-1] - dp[row + 1][-1])
+                dungeon[row][-1] = abs(dungeon[row][-1] - dungeon[row + 1][-1])
 
         for row in range(ROWS - 2, -1, -1):
             for col in range(COLS - 2, -1, -1):
-                if (dungeon[row][col] - dp[row][col + 1] >= 0) or (
-                    dungeon[row][col] - dp[row + 1][col] >= 0
+                if (dungeon[row][col] - dungeon[row][col + 1] >= 0) or (
+                    dungeon[row][col] - dungeon[row + 1][col] >= 0
                 ):
-                    dp[row][col] = 1
+                    dungeon[row][col] = 1
                 else:
-                    dp[row][col] = abs(dungeon[row][col] - min(dp[row][col + 1], dp[row + 1][col]))
+                    dungeon[row][col] = abs(dungeon[row][col] - min(dungeon[row][col + 1], dungeon[row + 1][col]))
 
-        return dp[0][0]
+        return dungeon[0][0]
+
+assert Solution().calculateMinimumHP([[-2,-3,3],[-5,-10,1],[10,30,-5]]) == 7
+assert Solution().calculateMinimumHP([[0]]) == 1
+assert Solution().calculateMinimumHP([[0,0,0],[1,1,-1]]) == 1
+assert Solution().calculateMinimumHP([[-3,5]]) == 4
