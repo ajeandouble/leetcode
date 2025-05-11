cc05d00ff7193d42f6c10dd9c339487998d8ad56
ajeandouble <jean.adrien.mtpl@gmail.com>
2025-05-11 16:28:34 +0200

    Keep track of scores for Stone Game III

--- 1406. Stone Game III/solution1406.py
+++ 1406. Stone Game III/solution1406.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from typing import List
+
 
 class Solution:
     def stoneGameIII(self, stones: List[int]) -> str:
@@ -35,3 +37,31 @@ assert Solution().stoneGameIII([1, 2, 3, 6]) == "Tie"
 assert Solution().stoneGameIII([1, 2, 3, -9]) == "Alice"
 assert Solution().stoneGameIII([1, 2, 3, -9]) == "Alice"
 assert Solution().stoneGameIII([-1, -2, -3]) == "Tie"
+
+# class Solution:
+#     def stoneGameIII(self, stones: List[int]) -> str:
+#         N = len(stones)
+#         memo = {}
+
+#         def dfs(l):
+#             if l >= N:
+#                 return 0
+#             if l in memo:
+#                 return memo[l]
+#             max_delta = float("-inf")
+#             for n_stones in range(3):
+#                 take = sum(stones[l : l + n_stones + 1])
+#                 delta = take - dfs(l + n_stones + 1)
+#                 if delta >= max_delta:
+#                     max_delta = delta
+#             memo[l] = max_delta
+#             return memo[l]
+
+#         alice_delta = dfs(0)
+#         if alice_delta == 0:
+#             return "Tie"
+#         elif alice_delta < 0:
+#             return "Bob"
+#         else:
+#             return "Alice"
+#         return "Fuck"
